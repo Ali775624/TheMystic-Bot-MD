6a59c81b6461a49a15968b28f4b1767b27d51d11
القياده الخال شخصياً <115638470+Ali775624@users.noreply.github.com>
2023-03-01 04:24:23 +0300

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -6,11 +6,11 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 let name = await conn.getName(m.sender)
 let donar =`
 *┏ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
-*┇          「 𝐃𝐎𝐍𝐀𝐑 」*
+*┇     「 اهلين برو مطورني القياده الخال 」*
 *┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
-*┃ 𝙷𝙾𝙻𝙰 ${name}*
+*┃ هلا فيك ${name}*
 *┃*
-*┃ 👉🏻 𝙰𝚀𝚄𝙸 𝙰𝙻𝙶𝚄𝙽𝙾𝚂 𝙳𝙰𝚃𝙾𝚂*
+*┃ 👉🏻 اليك رقم هاتف مطورني اسفل*
 *┃ 𝙿𝙾𝚁 𝚂𝙸 𝙶𝚄𝚂𝚃𝙰𝚂 𝙰𝙿𝙾𝚈𝙰𝚁 :𝟹*
 *┃*
 *┃ ➤ 𝙲𝙻𝙰𝚅𝙴:* 
@@ -18,11 +18,11 @@ let donar =`
 *┃ ➤ 𝙱𝙰𝙽𝙲𝙾: STP* 
 *┃ ➤ 𝙱𝙴𝙽𝙴𝙵𝙸𝙲𝙸𝙰𝚁𝙸𝙾: Bruno Sobrino* 
 *┃ ➤ 𝙲𝙾𝙽𝙲𝙴𝙿𝚃𝙾: APOYO*  
-*┃ ➤ 𝙿𝙰𝚈𝙿𝙰𝙻: https://www.paypal.me/TheShadowBrokers133*
+*┃ ➤ شرح استخدام البوت: https://youtu.be/ViXwP-DgWuU*
 *┃*
 *┃ 👉🏻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙰𝙼𝙴 𝚂𝙸 𝙽𝙴𝙲𝙴𝚂𝙸𝚃𝙰𝚂*
 *┃ 𝙼𝙰𝚂 𝙳𝙰𝚃𝙾𝚂 𝚈 𝙿𝙰𝚁𝙰 𝙰𝙶𝚁𝙰𝙳𝙴𝙲𝙴𝚁𝚃𝙴*
-*┃ wa.me/5219992095479*
+*┃ wa.me/967711023486*
 *┗ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
 `.trim()
 let aa = { quoted: m, userJid: conn.user.jid }
