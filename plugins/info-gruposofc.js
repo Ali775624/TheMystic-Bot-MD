f53814b4c7b67d4488080357796062f2a68301bc
القياده الخال شخصياً <115638470+Ali775624@users.noreply.github.com>
2023-03-01 04:33:24 +0300

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,17 +1,17 @@
 let media = imagen4
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
-*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 𝚃𝙷𝙴 𝚂𝙷𝙰𝙳𝙾𝚆 𝙱𝚁𝙾𝙺𝙴𝚁𝚂 - 𝚃𝙴𝙰𝙼 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
+*شبيك لبيك اشياء البوت وتحديثاته بين يديك يمكنك استخدام البوت بتقنيه وسيعه مطور بحتراف وتقنيه من قبل الخال شخصياً اليك اسفل قائمة تحديثات البوت وشرح عن كيفية استخدام البوت :D*
 
-*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*1.-* https://chat.whatsapp.com/FWmPWnVqpiQ4XNpLN98g3G
+*➤ قاىمة تحديثات البوت واضافات البوت:*
+*1.-* قناة التحديثات تليجرام  https://t.me/Driving_uncle_personally
 
-*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
+*2.-* طريقة استخدام البوت شرح يوتيوب https://youtu.be/ViXwP-DgWuU
 
-*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
+*3.-* حسابي تيك توك https://www.tiktok.com/@alloush_alyamanl?_t=8XH4ksw51Ji&_r=1
 
-*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
+*4.-* حسابي انستجرام https://www.instagram.com/lqydhlkhlshkhsyan?r=nametag
 
-*5.-* https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb
+*5.-* اهم قناه مختص بـ ادوات تريمكس خطيره https://t.me/Termux7
 `.trim(), wm, media, [['💟 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 💟', '#menu']], m)
 handler.command = /^linkgc|grupos$/i
 export default handler
